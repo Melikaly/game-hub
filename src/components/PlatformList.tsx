94c1d524bf9d99d876a293dd4f2e935eb695f7e7
Melika Arab <melikaly19@gmail.com>
2025-08-23 11:37:56 +0330

    Displying Platform Icons

---
+++ src/components/PlatformList.tsx
@@ -0,0 +1,43 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
+
+interface Prop {
+  platforms: Platform[];
+}
+
+const PlatformList = ({ platforms }: Prop) => {
+  const iconMap: { [key: string]: IconType } = {
+    //name : PlayStation
+    //slug : playstation
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color={"gray.500"}></Icon>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformList;
