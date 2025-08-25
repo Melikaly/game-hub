8dfb7e982a4b93a3afdc115969d16661eb29db99
Melika Arab <melikaly19@gmail.com>
2025-08-25 11:12:29 +0330

    Fetch the geners

---
+++ src/components/GenerList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import useGeners from '../hooks/useGeners'
+
+const GenerList = () => {
+    const {geners} = useGeners()
+  return (
+    <ul>
+        {geners.map(gener => <li key={gener.id}>{gener.name}</li>)}
+    </ul> 
+  )
+}
+
+export default GenerList
