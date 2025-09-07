6139b99f80bb99cdbf97946cb1a8019cf49d4d0a
Melika Arab <melikaly19@gmail.com>
2025-09-07 12:36:24 +0330

    update api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api/',
-    params: {
-        key : '2f2a113bf942449da0a56a45c32e3de1'
-    }
-})
+  baseURL: "https://api.rawg.io/api/",
+  params: {
+    key: "023522e538b24fe9b7b9ef52a810dc16",
+  },
+});
